95c42e4b4564549a6a0cca6713832ed2c0ca9560
John Fallara <john@fallara.net>
2017-01-13 15:16:58 -0600

    "Plugins" module, part 1

--- js/site.js
+++ js/site.js
@@ -5,6 +5,8 @@
 (function() {
     "use strict";
 
+    // Dropdown Button
+
     var $pickButton = $("#pickButton");
 
     $("#houseDropdown li a").on("click", function() {
@@ -16,5 +18,31 @@
         $pickButton.addClass("active");
 
     });
+
+    // Modal Events
+
+    var $sentDialog = $("#sentDialog");
+
+    $("#contactForm").on("submit", function() {
+        $sentDialog.modal("show");
+        return false;
+    });
+
+    var $sentAlert = $("#sentAlert");
+
+    $sentDialog.on("hidden.bs.modal", function() {
+        //alert("modal closed");
+        $sentAlert.show();
+    });
+
+        // Overrides a peculiarity of alerts that normally makes them only appear once
+    $sentAlert.on("close.bs.alert", function() {
+        $sentAlert.hide();
+        return false;
+    });
+
+    // Enable Tooltip
+
+    $("#contactForm input[type=submit]").tooltip();
 
 })();
